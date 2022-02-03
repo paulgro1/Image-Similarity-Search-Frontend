8147a9c53aa616ffef1413c1fa308cd3cf10da01
s75934 <s75934@bht-berlin.de>
2022-02-03 12:55:26 +0100

    new button in navbar

--- iss-frontend/src/components/InstructionsButton.js
+++ iss-frontend/src/components/InstructionsButton.js
@@ -68,99 +68,177 @@ class InstructionsButton extends Component {
         return (
             <div>
                 <Button variant="outline-success" onClick={this.handleShow}> 
-                    <Gear/>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                  <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                </svg>
                 </Button>
 
-                <Modal show={showDialog} onHide={this.handleClose} size='xl'>
+                <Modal show={showDialog} onHide={this.handleClose} >
                     <Modal.Header closeButton>
                         <Modal.Title>Instructions</Modal.Title>
                     </Modal.Header>
                     <Modal.Body className="show-grid">
         <Container>
           
+        <table class="table table-striped">
+
+        <div class="container-fluid" id="bordercontainer">
+
+
+       <div class="row">
           <Row>
+
+          <div class="col">
+
           <Col xs={6} md={4}>
           <div class="col-xs-1" align="center">
           <img src={scrollimage} height={50} width={50} alt="scroll icon" />
           </div>
            </Col>
+          </div>
+
+          <div class="col">
           <Col xs={12} md={8}>
               Zoom in and out
             </Col>
+            </div>
+
           </Row>
 
+          </div>
+
+
+
+
+          <div class="row">
 
             <Row>
+            <div class="col">
             <Col xs={6} md={4}>
             <div class="col-xs-1" align="center">
             <img src={leftclickandpan} height={50} width={50} alt="left click and pen icon" />
             </div>
             </Col>
+            </div>
+
+            <div class="col">
           <Col xs={12} md={8}>
               Slide through the dataset
             </Col>
+            </div>
             </Row>
 
+            </div>
+
+
+          <div class="row">
 
           <Row>
+          <div class="col">
             <Col xs={6} md={4}>
             <div class="col-xs-1" align="center">
             <img src={leftclick} height={50} width={50} alt="left click icon"/>
             </div>
             </Col>
+            </div>
+
+            <div class="col">
             <Col xs={12} md={8}>
               Click to get the nearest neighbours
             </Col>
+            </div>
           </Row>
+
+          </div>
             
 
+          <div class="row">
+
             <Row>
+            <div class="col">
             <Col xs={6} md={4}>
             <div class="col-xs-1" align="center">
             <img src={leftclick} height={50} width={50} alt=" double left click icon" />
             </div>
             </Col>
+            </div>
+
+            <div class="col">
             <Col xs={12} md={8}>
-              Double Click to get the detail view, nearest neighbours, similarity, option to export data
+              Two Clicks on marked image to get the detail view, nearest neighbours, similarity, option to export data
             </Col>
+            </div>
             </Row>
 
+            </div>
+
+
+            <div class="row">
 
             <Row>
+            <div class="col">
             <Col xs={6} md={4}>
             <div class="col-xs-1" align="center">
             <img src={settingsButton} height={50} width={50} alt="setting buttom"/>
             </div>
             </Col>
+            </div>
+
+            <div class="col">
             <Col xs={12} md={8}>
-              Click here to set number of nearest neighbours with a Slider
+              Click here to set number of nearest neighbours and/or  with a Slider
             </Col>
+            </div>
             </Row>
-           
+
+            </div>
+
+
+           <div class="row">
 
             <Row>
+            <div class="col">
             <Col xs={6} md={4}>
             <div class="col-xs-1" align="center">
             <img src={exportButton} height={50} width={50} alt="export button"/>
             </div>
             </Col>
+            </div>
+
+            <div class="col">
             <Col xs={12} md={8}>
               Click here to export (clicked) nearest neighbours in a table
             </Col>
+            </div>
             </Row>
 
+            </div>
+
+
+            <div class="row">
 
             <Row>
+            <div class="col">
             <Col xs={6} md={4} >
+
             <div class="col-xs-1" align="center">
             <img src={uploadImageButton} height={50} width={120} alt="uploaded image button"/>
             </div>
+
             </Col>
+            </div>
+
+            <div class="col">
             <Col xs={12} md={8}>
               Click here to choose between Single- and Multiupload
             </Col>
+            </div>
             </Row>
-    
+            </div>
+
+            </div>
+    </table>
         </Container>
       </Modal.Body>
                     <Modal.Footer>
