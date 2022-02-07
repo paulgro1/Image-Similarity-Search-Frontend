df722e7d01bfcc757ac09db13cb7b09b9e4c0d58
Fabian Löffler <s78278@beuth-hochschule.de>
2022-02-07 19:16:26 +0100

    style changes

--- iss-frontend/src/components/InstructionsButton.js
+++ iss-frontend/src/components/InstructionsButton.js
@@ -79,7 +79,7 @@ class InstructionsButton extends Component {
         return (
             <div >
                 <Tooltip id="tooltip" title="Instructions" placement="bottom">
-                    <div>
+                    <div id="settingButton">
                       <Button variant="outline-success" onClick={this.handleShow}> 
                           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
                             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
@@ -115,7 +115,7 @@ class InstructionsButton extends Component {
                   <img id="attentionIcon" src={warning} height={20}  alt="scroll icon" />
                   The settings can not be changed while images are marked.<br></br><br></br>
                   <img id="attentionIcon" src={warning} height={20} alt="scroll icon" />
-                  To reset the marking of the images, click on the white area around them.
+                  Reset the marked images, by clicking on the canvas.
                     
                     </div>
         </Row>
